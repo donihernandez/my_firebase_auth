ceeb5f11be52f48e813871c0fac46a4d44b3b6df
Adonai Domingez <donihernandez003@gmail.com>
2022-10-01 09:44:56 -0400

    initial commit

---
+++ src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+    return (
+        <div>
+            <h1 className='text-center text-3xl font-bold'>
+                Autenticación con Firebase & Context API
+            </h1>
+        </div>
+    )
+}
+
+export default Signup
