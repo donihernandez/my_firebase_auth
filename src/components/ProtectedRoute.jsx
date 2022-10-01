6eb8bd52a4cf00deb12218e21ecfbf0ac488f5ec
Adonai Domingez <donihernandez003@gmail.com>
2022-10-01 11:41:07 -0400

    fix import

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import { useUserContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = UserAuth();
+  const { user } = useUserContext();
 
   if (!user) {
     return <Navigate to='/' />;
